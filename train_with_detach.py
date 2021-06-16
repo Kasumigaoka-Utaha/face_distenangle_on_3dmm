b9572043693961698bc9d6a41acf6fee5a34d93f
Kasumigaoka-Utaha <45712215+Kasumigaoka-Utaha@users.noreply.github.com>
2021-06-16 23:53:38 +0800

    Update train_with_detach.py

--- train_with_detach.py
+++ train_with_detach.py
@@ -27,13 +27,13 @@ class Average_data(object):
         self.count += n
         self.avg = self.sum / self.count
 
-mouthIdx = range(31,)
-otherIdx = range(0,30)
+mouthIdx = range(31,51)
+otherIdx = range(0,31)
 def train(epoch,id,focal,exp_list,exp_dis,exp_cat,face_proj,lands_info,optimizer_dis,optimizer_cat,scheduler_dis,scheduler_cat):
-    rounds = 2000
+    rounds = 1000
     length = len(exp_list)
     loss_data = Average_data()
-    id = torch.as_tensor(id).cuda()
+    id = id.cuda()
     focal = torch.as_tensor(focal).cuda()
     with tqdm(range(rounds)) as _tqdm:
         for round in _tqdm:
@@ -49,10 +49,10 @@ def train(epoch,id,focal,exp_list,exp_dis,exp_cat,face_proj,lands_info,optimizer
             with torch.no_grad():
                 _,out_2 = exp_dis(exp_para_2)
             exp_out = exp_cat(out_1_1,out_2)
-            exp_out += exp_para_1
+            #exp_out += exp_para_1
             geometry_with_expNet_1 = face_proj.forward_geo_sub(id, exp_out, lands_info[-51:].long())
-            geometry_without_expNet_1 = face_proj.forward_geo_sub(id, exp_para_1, lands_info[-51:].long())
-            geometry_without_expNet_2 = face_proj.forward_geo_sub(id, exp_para_2, lands_info[-51:].long())
+            with torch.no_grad():
+                geometry_without_expNet_1 = face_proj.forward_geo_sub(id, exp_para_1, lands_info[-51:].long())
             loss_lan_others = cal_lan_loss(geometry_with_expNet_1[:, otherIdx, :2], geometry_without_expNet_1[:, otherIdx, :2])
             # detach o1 side
             out_2_1,out_2_2 = exp_dis(exp_para_2)
@@ -60,10 +60,10 @@ def train(epoch,id,focal,exp_list,exp_dis,exp_cat,face_proj,lands_info,optimizer
             with torch.no_grad():
                 out_1,_ = exp_dis(exp_para_1)
             exp_out = exp_cat(out_1,out_2_2)
-            exp_out += exp_para_1
+            #exp_out += exp_para_1
             geometry_with_expNet_2 = face_proj.forward_geo_sub(id, exp_out, lands_info[-51:].long())
-            geometry_without_expNet_1 = face_proj.forward_geo_sub(id, exp_para_1, lands_info[-51:].long())
-            geometry_without_expNet_2 = face_proj.forward_geo_sub(id, exp_para_2, lands_info[-51:].long())
+            with torch.no_grad():
+                geometry_without_expNet_2 = face_proj.forward_geo_sub(id, exp_para_2, lands_info[-51:].long())
             loss_lan_mouth = cal_lan_loss(geometry_with_expNet_2[:, mouthIdx, :2], geometry_without_expNet_2[:, mouthIdx, :2])
             loss_lan = loss_lan_mouth+loss_lan_others
             loss_data.update(loss_lan.item(), 1)
@@ -77,10 +77,10 @@ def train(epoch,id,focal,exp_list,exp_dis,exp_cat,face_proj,lands_info,optimizer
     scheduler_dis.step()
 
 def test(epoch,id,focal,exp_list,exp_dis,exp_cat,face_proj,lands_info,best_loss):
-    rounds = 500
+    rounds = 200
     length = len(exp_list)
     loss_data = Average_data()
-    id = torch.as_tensor(id).cuda()
+    id = id.cuda()
     focal = torch.as_tensor(focal).cuda()
     with torch.no_grad():
         with tqdm(range(rounds)) as _tqdm:
@@ -94,7 +94,7 @@ def test(epoch,id,focal,exp_list,exp_dis,exp_cat,face_proj,lands_info,best_loss)
                 out_1,_ = exp_dis(exp_para_1)
                 _,out_2 = exp_dis(exp_para_2)
                 exp_out = exp_cat(out_1,out_2)
-                exp_out += exp_para_1
+                #exp_out += exp_para_1
                 geometry_with_expNet = face_proj.forward_geo_sub(id, exp_out, lands_info[-51:].long())
                 geometry_without_expNet_1 = face_proj.forward_geo_sub(id, exp_para_1, lands_info[-51:].long())
                 geometry_without_expNet_2 = face_proj.forward_geo_sub(id, exp_para_2, lands_info[-51:].long())
@@ -113,19 +113,19 @@ def test(epoch,id,focal,exp_list,exp_dis,exp_cat,face_proj,lands_info,best_loss)
             'net': exp_dis.state_dict(),
             'epoch': epoch
         }
-        torch.save(state_dis, './checkpoint/dis_ckpt.pth')
+        torch.save(state_dis, './checkpoint/dis_with_detach_ckpt.pth')
         state_cat = {
             'net': exp_cat.state_dict(),
             'epoch': epoch
         }
-        torch.save(state_cat, './checkpoint/cat_ckpt.pth')
+        torch.save(state_cat, './checkpoint/cat_with_detach_ckpt.pth')
 
         
 
 
 def main():
     # configuration
-    train_epoch = 200
+    train_epoch = 50
     TRAIN_LR = 0.05
     TRAIN_MOMENTUM = 0.9
     TRAIN_WEIGHT_DECAY = 5e-4
@@ -135,7 +135,7 @@ def main():
     lands_info = torch.as_tensor(lands_info).cuda()
     # load data
     path = './face_3dmm_params/obama/'
-    id,focal = data_loader.load_id(os.path.join(path,'static_params.json'))
+    _,focal = data_loader.load_id(os.path.join(path,'static_params.json'))
     exp_list,_,_ = data_loader.load_exp(path)
     exp_dis = network.Distangler(79,128,64)
     exp_cat = network.Concatenater(128,64,79)
@@ -143,13 +143,14 @@ def main():
     exp_dis = exp_dis.to(device)
     exp_cat = exp_cat.to(device)
     id_dim, exp_dim, tex_dim, point_num = 100, 79, 100, 34650
+    id = torch.zeros([1, id_dim],dtype = torch.float)
     face_proj = Face_3DMM('./3DMM',id_dim, exp_dim, tex_dim, point_num)
     optimizer_dis = optim.SGD(exp_dis.parameters(), lr=TRAIN_LR,momentum=TRAIN_MOMENTUM,weight_decay=TRAIN_WEIGHT_DECAY)
     optimizer_cat = optim.SGD(exp_cat.parameters(), lr=TRAIN_LR,momentum=TRAIN_MOMENTUM,weight_decay=TRAIN_WEIGHT_DECAY)
     scheduler_dis = StepLR(optimizer_dis, step_size=TRAIN_LR_DECAY_STEP, gamma=TRAIN_LR_DECAY_RATE)
     scheduler_cat = StepLR(optimizer_cat, step_size=TRAIN_LR_DECAY_STEP, gamma=TRAIN_LR_DECAY_RATE)
     best_loss = 100
-    for epoch in range(1,train_epoch):
+    for epoch in range(1,train_epoch+1):
         print('Epoch: %d.'%(epoch))
         train(epoch,id,focal,exp_list,exp_dis,exp_cat,face_proj,lands_info,optimizer_dis,optimizer_cat,scheduler_dis,scheduler_cat)
         test(epoch,id,focal,exp_list,exp_dis,exp_cat,face_proj,lands_info,best_loss)
